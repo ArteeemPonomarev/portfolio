cb6328e2b1dc8554bfa05d3e8bda7481a6788725
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-04-04 03:26:16 +0400

    create header's content and start use flex

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import style from './Header.module.css'
+import style from './Header.module.css';
+import Navmenu from './Navmenu';
 
 function Header() {
   return (
     <div className={style.header}>
- 
+      <div className={style.container}>
+        <Navmenu />
+      </div>
     </div>
   );
 }
