c2268d0e34c42d27b4ed4749f8b2ddc1934e6580
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-04-04 04:17:50 +0400

    add greetings and photo

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,7 +4,16 @@ import style from './Main.module.css'
 function Main() {
   return (
     <div className={style.main}>
- 
+      <div className={style.container}>
+        <div className={style.greeting}>
+          <span>Привет</span>
+          <span>Меня зовут Артем Пономарев</span>
+          <span>Я front-end разработчик</span>
+        </div>
+        <div className={style.photo}>
+          <img src="" alt=""/>
+        </div>
+      </div>
     </div>
   );
 }
