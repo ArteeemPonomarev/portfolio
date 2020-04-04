6590cfc464ce011fdcb58a20bbc6ea26873b26e2
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-04-04 05:00:58 +0400

    create content

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -4,6 +4,37 @@ import style from './Skills.module.css'
 function Skills() {
   return (
     <div className={style.skills}>
+      <div className={style.container}>
+        <div className={style.title}>
+          My skills
+        </div>
+        <div className={style.skills_list}>
+          <div className={style.skill_item}>
+            <div className={style.skill_item_image}>
+            </div>
+            <div className={style.technology_name}>React</div>
+            <div className={style.description}>
+              <span>Подробное описание навыка</span>
+            </div>
+          </div>
+          <div className={style.skill_item}>
+            <div className={style.skill_item_image}>
+            </div>
+            <div className={style.technology_name}>React</div>
+            <div className={style.description}>
+              <span>Подробное описание навыка</span>
+            </div>
+          </div>
+          <div className={style.skill_item}>
+            <div className={style.skill_item_image}>
+            </div>
+            <div className={style.technology_name}>React</div>
+            <div className={style.description}>
+              <span>Подробное описание навыка</span>
+            </div>
+          </div>
+        </div>
+      </div>
  
     </div>
   );
